f6a68472602871d119ec473cbea8f68f029d63cf
Maxime PEREZ <maxime_perez_2@ext.carrefour.com>
2024-09-25 20:56:43 +0200

    day 4

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,8 +1,25 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
 
+use std::ops::Add;
+
+#[derive(Debug)]
+#[derive(Clone)]
 pub struct WrappingU32 {
     value: u32,
+}
+
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+    fn add(self, other:WrappingU32) -> WrappingU32 {
+        WrappingU32{value: self.value.wrapping_add(other.value)}
+    }
+}
+
+impl PartialEq for WrappingU32 {
+    fn eq(&self, other: &WrappingU32) -> bool {
+        self.value == other.value
+    }
 }
 
 impl WrappingU32 {
@@ -20,6 +37,6 @@ mod tests {
         let x = WrappingU32::new(42);
         let y = WrappingU32::new(31);
         let z = WrappingU32::new(u32::MAX);
-        assert_eq!(x + y + y + z, WrappingU32::new(103));
+        assert_eq!(x + y.clone() + y + z, WrappingU32::new(103));
     }
 }
